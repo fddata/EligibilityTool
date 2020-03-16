c1ea29a14924e4ef67edda2d16415e11e86aa62d
Fraser Douglas <fraser.j.douglas@gmail.com>
2020-03-16 23:31:39 +0000

    adds Card and CardApplication models

---
+++ Models/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace EligibilityTool.Models
+{
+    public class Card
+    {
+        public int CardID { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal APR { get; set; }
+
+        public string PromoMessage { get; set; }
+
+    }
+}
---
+++ Models/CardApplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EligibilityTool.Models
+{
+    public class CardApplication
+    {
+
+        public int CardApplicationID { get; set; }
+
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+
+        public DateTime DOB { get; set; }
+
+        public decimal Income { get; set; }
+
+        public virtual ICollection<Card> Cards { get; set; }
+
+    }
+}

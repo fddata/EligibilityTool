5816e7e5f9e92917e5059501170f9ecf2f266f15
Fraser Douglas <fraser.j.douglas@gmail.com>
2020-03-16 23:36:22 +0000

    adds DAL folder to set up db context (Data Access Layer)

---
+++ DAL/EligibilityToolContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using EligibilityTool.Models;
+
+namespace EligibilityTool.DAL
+{
+    public class EligibilityToolContext : DbContext
+    {
+
+        public EligibilityToolContext() : base("EligibilityToolContext") 
+        { 
+        }
+
+        public DbSet<Card> Cards { get; set; }
+
+        public DbSet<CardApplication> CardApplications { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}

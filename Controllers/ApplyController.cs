76bbd2bad3cb08b78c005dbfb9684ea294b99ec6
Fraser Douglas <fraser.j.douglas@gmail.com>
2020-03-17 20:20:23 +0000

    adds GetEligibleCards method

--- Controllers/ApplyController.cs
+++ Controllers/ApplyController.cs
@@ -1,4 +1,6 @@
 ﻿using EligibilityTool.DAL;
+using EligibilityTool.Models;
+using EligibilityTool.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +15,65 @@ namespace EligibilityTool.Controllers
         private EligibilityToolContext db = new EligibilityToolContext();
 
         // GET: Apply
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(ApplyViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+
+                var eligibleCards = GetEligibleCards(model);
+
+            }
+
+        }
+
+
+        private List<Card> GetEligibleCards(ApplyViewModel model)
+        {
+            var result = new List<Card>() { };
+            try
+            {
+                if (model.DOB.AddYears(18) > DateTime.Now)
+                {
+                    //applicant is less than 18, return empty list
+                    return result;
+                }
+                else if (model.Income < 30000m)
+                {
+                    result.Add(db.Cards.First(c => c.Name == "Vanquis"));
+                    return result;
+                }
+                else
+                {
+                    result.Add(db.Cards.First(c => c.Name == "BarclayCard"));
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"GetEligibleCards failed with message: {e.Message}");
+                //return empty list
+                return result;
+            }
+
+        }
+
+
+
+
+
+
     }
 }

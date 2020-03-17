eac9b85a34d55e92ee3553345d6bff6543ad0769
Fraser Douglas <fraser.j.douglas@gmail.com>
2020-03-17 19:51:45 +0000

    adds ApplyViewModel class

---
+++ Models/ViewModels/ApplyViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EligibilityTool.Models.ViewModels
+{
+    public class ApplyViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Date Of Birth")]
+        [DisplayFormat (ApplyFormatInEditMode =true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DOB { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Income (GBP p.a.")]
+        public decimal Income { get; set; }
+    }
+}

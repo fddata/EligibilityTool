5bed730f23e72219b774f57db04abc4578eba1b2
Fraser Douglas <fraser.j.douglas@gmail.com>
2020-03-17 19:45:56 +0000

    adds apply controller

---
+++ Controllers/ApplyController.cs
@@ -0,0 +1,21 @@
+﻿using EligibilityTool.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EligibilityTool.Controllers
+{
+    public class ApplyController : Controller
+    {
+
+        private EligibilityToolContext db = new EligibilityToolContext();
+
+        // GET: Apply
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
